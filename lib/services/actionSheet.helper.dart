da5e73080f5df4804a55df00063158e806663be2
Mahmoud Nabhan <niypoo@gmail.com>
2024-01-28 16:27:01 +0200

    Update actionSheet.helper.dart

--- lib/services/actionSheet.helper.dart
+++ lib/services/actionSheet.helper.dart
@@ -2,7 +2,6 @@ import 'package:bottom_sheet_helper/models/actionSheetOption.model.dart';
 import 'package:bottom_sheet_helper/services/customBottomSheet.helper.dart';
 import 'package:flutter/material.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
-import 'package:fly_ui/views/widgets/tableRow.widget.dart';
 import 'package:get/get.dart';
 import 'package:unicons/unicons.dart';
 
@@ -27,41 +26,41 @@ class ActionSheetHelper {
                 style: Get.textTheme.bodyLarge,
               ),
             )
-          : SingleChildScrollView(
-              child: Column(
-                children: options.map(
-                  (option) {
-                    return ListTile(
-                      contentPadding: EdgeInsets.all(5.sp),
-                      onTap: () => Get.back(result: option.value),
-                      leading: option.leading,
-                      title: Text(
-                        option.title,
-                        style: Get.textTheme.labelMedium!.copyWith(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 10.sp,
-                        ),
-                        maxLines: 2,
-                      ),
-                      subtitle: Text(
-                        option.subtitle ?? ' ',
-                        style: Get.textTheme.bodySmall!.copyWith(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 9.sp,
-                        ),
-                        maxLines: 2,
-                      ),
-                      trailing:
-                          currentValue.toString() == option.value.toString()
-                              ? Icon(
-                                  UniconsLine.check,
-                                  color: Get.theme.primaryColor,
-                                )
-                              : null,
-                    );
-                  },
-                ).toList(),
-              ),
+          : ListView.separated(
+              itemBuilder: (c, i) {
+                return ListTile(
+                  contentPadding: EdgeInsets.all(5.sp),
+                  onTap: () => Get.back(result: options[i].value),
+                  leading: options[i].leading,
+                  title: Text(
+                    options[i].title,
+                    style: Get.textTheme.labelMedium!.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 10.sp,
+                    ),
+                    maxLines: 2,
+                  ),
+                  subtitle: Text(
+                    options[i].subtitle ?? ' ',
+                    style: Get.textTheme.bodySmall!.copyWith(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 9.sp,
+                    ),
+                    maxLines: 2,
+                  ),
+                  trailing:
+                      currentValue.toString() == options[i].value.toString()
+                          ? Icon(
+                              UniconsLine.check,
+                              color: Get.theme.primaryColor,
+                            )
+                          : null,
+                );
+              },
+              separatorBuilder: (c, i) => i == options.length
+                  ? const SizedBox.shrink()
+                  : const Divider(),
+              itemCount: options.length,
             ),
       color: color,
       height: height,
