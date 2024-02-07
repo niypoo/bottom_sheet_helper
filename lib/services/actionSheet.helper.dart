4afb91e01b017512267e9e7f8ec973c3e5ce7b9d
Mahmoud Nabhan <niypoo@gmail.com>
2024-02-07 17:15:53 +0200

    Update actionSheet.helper.dart

--- lib/services/actionSheet.helper.dart
+++ lib/services/actionSheet.helper.dart
@@ -34,13 +34,7 @@ class ActionSheetHelper {
                   minVerticalPadding: 0,
                   contentPadding: EdgeInsets.symmetric(horizontal: 5.sp),
                   onTap: () => Get.back(result: options[i].value),
-                  leading: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      if (options[i].leading != null) options[i].leading!,
-                    ],
-                  ),
+                  leading: options[i].leading,
                   title: Text(
                     options[i].title,
                     style: Get.textTheme.labelMedium!.copyWith(
