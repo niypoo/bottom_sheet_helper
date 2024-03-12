7f2a3809e083a43b2aa467992208c7d1220f3570
Mahmoud Nabhan <niypoo@gmail.com>
2024-03-12 15:08:43 +0200

    Update porrait.layout.dart

--- lib/views/layouts/porrait.layout.dart
+++ lib/views/layouts/porrait.layout.dart
@@ -45,8 +45,8 @@ class BottomSheetPortraitLayout extends StatelessWidget {
             left: 10.sp,
             bottom: 5.sp,
           ),
-          width: context.width * 0.95,
-          margin: EdgeInsets.only(bottom: context.height * 0.03, top: 30),
+          width: context.width - 30.sp,
+          margin: EdgeInsets.symmetric(vertical: 30.sp),
           height: height,
           child: SingleChildScrollView(
             child: Column(
