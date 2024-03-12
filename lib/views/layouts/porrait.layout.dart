4c82b35c9142eba6047e8cc983ee09ca18c285a1
Mahmoud Nabhan <niypoo@gmail.com>
2024-03-12 15:30:14 +0200

    Update porrait.layout.dart

--- lib/views/layouts/porrait.layout.dart
+++ lib/views/layouts/porrait.layout.dart
@@ -1,6 +1,5 @@
 import 'package:bottom_sheet_helper/views/widgets/header.widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:get/get.dart';
 import 'package:bottom_sheet_helper/views/widgets/draggableButton.widget.dart';
@@ -31,61 +30,61 @@ class BottomSheetPortraitLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: <Widget>[
-        // Body Of Sheet
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 15.sp , horizontal: 10.sp),
-          color: Colors.transparent,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15.sp),
-              color: color ?? Get.theme.scaffoldBackgroundColor,
-            ),
-            padding: EdgeInsets.only(
-              top: 10.sp,
-              right: 10.sp,
-              left: 10.sp,
-              bottom: 5.sp,
-            ),
-            width: double.maxFinite,
-            height: height,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // HEADER
-                  if (subTitle != null || title != null)
-                    BottomSheetHeader(title: title, subTitle: subTitle),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 15.sp, horizontal: 5.sp),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.sp),
+          color: color ?? Get.theme.scaffoldBackgroundColor,
+        ),
+        padding: EdgeInsets.only(
+          top: 10.sp,
+          right: 10.sp,
+          left: 10.sp,
+          bottom: 5.sp,
+        ),
+        width: double.maxFinite,
+        height: height,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // HEADER
+              if (subTitle != null || title != null)
+                BottomSheetHeader(title: title, subTitle: subTitle),
 
-                  // body
-                  Flexible(
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                        bottom: 5.sp,
-                      ),
-                      child: child,
-                    ),
+              // body
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 5.sp,
                   ),
-
-                  // Footer
-                  if (footer != null) footer!
-                ],
+                  child: child,
+                ),
               ),
-            ),
+
+              // Footer
+              if (footer != null) footer!
+            ],
           ),
         ),
-
-        // Draggable Button
-        if (draggable!)
-          DraggableButton(
-            icon: icon,
-            color: color,
-            iconColor: iconColor,
-          ),
-      ],
+      ),
     );
+
+    // return Stack(
+    //   alignment: Alignment.topCenter,
+    //   children: <Widget>[
+    //     // Body Of Sheet
+
+    //     // Draggable Button
+    //     // if (draggable!)
+    //     //   DraggableButton(
+    //     //     icon: icon,
+    //     //     color: color,
+    //     //     iconColor: iconColor,
+    //     //   ),
+    //   ],
+    // );
   }
 }
