e394ea8ff9429583f472df9e53994b5b4964a235
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-24 08:30:01 +0200

    Update actionSheet.helper.dart

--- lib/services/actionSheet.helper.dart
+++ lib/services/actionSheet.helper.dart
@@ -1,7 +1,6 @@
 import 'package:bottom_sheet_helper/models/actionSheetOption.model.dart';
 import 'package:bottom_sheet_helper/services/customBottomSheet.helper.dart';
 import 'package:flutter/material.dart';
-import 'package:fly_ui/views/widgets/inkWell.widget.dart';
 import 'package:get/get.dart';
 import 'package:patata_responsive/patata_responsive.dart';
 import 'package:unicons/unicons.dart';
@@ -30,50 +29,33 @@ class ActionSheetHelper {
           : ListView.separated(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
-              padding: EdgeInsetsDirectional.only(start: 8.sp),
+              padding: const EdgeInsets.all(0),
               itemBuilder: (c, i) {
-                return FlyInkWell(
+                return ListTile(
+                  minVerticalPadding: 0,
+                  contentPadding: const EdgeInsets.all(0),
                   onTap: () => Get.back(result: options[i].value),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (options[i].leading != null)
-                        Padding(
-                          padding: EdgeInsetsDirectional.only(end: 5.sp),
-                          child: options[i].leading!,
-                        ),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              options[i].title,
-                              style: Get.textTheme.titleSmall,
-                              maxLines: 2,
-                            ),
-                            if (options[i].subtitle != null)
-                              Text(
-                                options[i].subtitle!,
-                                style: Get.textTheme.bodySmall,
-                                maxLines: 2,
-                              )
-                          ],
-                        ),
-                      ),
-                      if (currentValue.toString() ==
-                          options[i].value.toString())
-                        Padding(
-                          padding: EdgeInsetsDirectional.only(start: 5.sp),
-                          child: Icon(
-                            UniconsLine.check,
-                            color: Get.theme.primaryColor,
-                            size: 22.sp,
-                          ),
-                        ),
-                    ],
+                  leading: options[i].leading,
+                  title: Text(
+                    options[i].title,
+                    style: Get.textTheme.titleSmall,
+                    maxLines: 2,
                   ),
+                  subtitle: options[i].subtitle != null
+                      ? Text(
+                          options[i].subtitle ?? ' ',
+                          style: Get.textTheme.bodySmall,
+                          maxLines: 2,
+                        )
+                      : null,
+                  trailing:
+                      currentValue.toString() == options[i].value.toString()
+                          ? Icon(
+                              UniconsLine.check,
+                              color: Get.theme.primaryColor,
+                              size: 22.sp,
+                            )
+                          : null,
                 );
               },
               separatorBuilder: (c, i) => i == options.length
